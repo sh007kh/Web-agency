9dcd42e4a8b62881a5fd16d6ea935e95601d951f
shahab khodayar <sh007kh@gmail.com>
2022-01-22 16:02:12 +0330

    add fixed navbar function in js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 const navLinks = document.querySelector(".nav-links");
+const navLink = document.querySelectorAll(".nav-link");
 const navBtn = document.querySelectorAll(".nav-btn");
+const navBar = document.querySelector(".navbar");
 const navBtnContainer = document.querySelector(".nav-btn-container");
+// nav-btn toggle
 navBtn.forEach(function (btn) {
   btn.addEventListener("click", function () {
     navLinks.classList.toggle("show-links");
     navBtnContainer.classList.toggle("show-btn");
   });
 });
+// close nav-links
 window.addEventListener("resize", function () {
   if (window.innerWidth > 768) navLinks.classList.remove("show-links");
   if (window.innerWidth > 768) navBtnContainer.classList.remove("show-btn");
+});
+// fixed navbar
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 80) {
+    navBar.classList.add("fixed-navbar");
+  } else {
+    navBar.classList.remove("fixed-navbar");
+  }
+});
+navLink.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navLinks.classList.remove("show-links");
+    navBtnContainer.classList.toggle("show-btn");
+  });
 });
